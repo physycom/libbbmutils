707e5cc0b3941ac3a1efc4fcf9c3e9e450d46a1d
Roman Maccaruni <rommacc@yahoo.it>
2016-02-13 14:39:34 +0100

    add C++ compatibility, include forgotten spare files

--- src/math_lib.h
+++ src/math_lib.h
@@ -2,87 +2,95 @@
 #include <stdlib.h>
 #include <math.h>
 
-// 3d vector algebra
-typedef struct VEC3D { 
-	double x, y, z, mod; 
-} VEC3D;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-VEC3D set_vec3d(double, double, double );
+  // 3d vector algebra
+  typedef struct VEC3D {
+    double x, y, z, mod;
+  } VEC3D;
 
-void setmod_vec3d(VEC3D *);
+  VEC3D set_vec3d(double, double, double);
 
-void normalize_vec3d(VEC3D *);
+  void setmod_vec3d(VEC3D *);
 
-double prod_dot(VEC3D, VEC3D);
+  void normalize_vec3d(VEC3D *);
 
-VEC3D prod_cross(VEC3D, VEC3D);
+  double prod_dot(VEC3D, VEC3D);
 
-void multiply_vec3d(double, VEC3D *);
+  VEC3D prod_cross(VEC3D, VEC3D);
 
-
-// 3d matrix algebra
-typedef struct MAT3D { 
-	double xx, xy, xz, yx, yy, yz, zx, zy, zz; 
-} MAT3D;
-
-MAT3D set_mat3d(double, double, double, double, double, double, double, double, double);
-
-MAT3D transpose_mat3d(MAT3D);
-
-MAT3D product_mat3d(MAT3D, MAT3D);
-
-void multiply_mat3d(double, MAT3D *);
-
-MAT3D make_rotation_cs(VEC3D, double, double);
-
-MAT3D make_rotation(VEC3D, double);
-
-VEC3D rotate_vec3d(VEC3D, MAT3D);
-
-MAT3D rotate_mat3d(MAT3D, MAT3D);
+  void multiply_vec3d(double, VEC3D *);
 
 
-// 6d vector
-typedef struct VEC6D {
-  VEC3D a, g;
-} VEC6D;
+  // 3d matrix algebra
+  typedef struct MAT3D {
+    double xx, xy, xz, yx, yy, yz, zx, zy, zz;
+  } MAT3D;
 
-VEC6D set_vec6d(double , double , double , double , double, double);
+  MAT3D set_mat3d(double, double, double, double, double, double, double, double, double);
 
-VEC6D rotate_vec6d(VEC6D, MAT3D);
+  MAT3D transpose_mat3d(MAT3D);
 
+  MAT3D product_mat3d(MAT3D, MAT3D);
 
-// 6d matrix
-typedef struct MAT6D {
-  MAT3D A[2][2];
-} MAT6D;
+  void multiply_mat3d(double, MAT3D *);
 
-MAT6D set_mat6d(MAT3D, MAT3D, MAT3D, MAT3D);
+  MAT3D make_rotation_cs(VEC3D, double, double);
 
-MAT6D rotate_mat6d(MAT6D, MAT3D);
+  MAT3D make_rotation(VEC3D, double);
 
+  VEC3D rotate_vec3d(VEC3D, MAT3D);
 
-// 2d eigenvalue problem
-typedef struct EigenSys{
-	double a[2][2];
-	double l1,l2;
-	double v1[2], v2[2], u1[2], u2[2];
-} EigenSys;
-
-EigenSys eigs_2x2_sym_normalized(double, double);
-
-EigenSys eigs_2x2_sym(double[2][2]);
-
-double check_eigs(EigenSys);
+  MAT3D rotate_mat3d(MAT3D, MAT3D);
 
 
-// display function
-void print_vec3d(VEC3D, const char *);
+  // 6d vector
+  typedef struct VEC6D {
+    VEC3D a, g;
+  } VEC6D;
 
-void print_mat3d(MAT3D, const char *);
+  VEC6D set_vec6d(double, double, double, double, double, double);
 
-void print_vec6d(VEC6D, const char *);
+  VEC6D rotate_vec6d(VEC6D, MAT3D);
 
-void print_mat6d(MAT6D, const char *);
 
-void print_eigs(EigenSys, const char *);
+  // 6d matrix
+  typedef struct MAT6D {
+    MAT3D A[2][2];
+  } MAT6D;
+
+  MAT6D set_mat6d(MAT3D, MAT3D, MAT3D, MAT3D);
+
+  MAT6D rotate_mat6d(MAT6D, MAT3D);
+
+
+  // 2d eigenvalue problem
+  typedef struct EigenSys {
+    double a[2][2];
+    double l1, l2;
+    double v1[2], v2[2], u1[2], u2[2];
+  } EigenSys;
+
+  EigenSys eigs_2x2_sym_normalized(double, double);
+
+  EigenSys eigs_2x2_sym(double[2][2]);
+
+  double check_eigs(EigenSys);
+
+
+  // display function
+  void print_vec3d(VEC3D, const char *);
+
+  void print_mat3d(MAT3D, const char *);
+
+  void print_vec6d(VEC6D, const char *);
+
+  void print_mat6d(MAT6D, const char *);
+
+  void print_eigs(EigenSys, const char *);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/test_3d.c
@@ -0,0 +1,36 @@
+#ifdef _MSC_VER
+#define _USE_MATH_DEFINES
+#endif
+
+#include "math_lib.h"
+
+int main() {
+  VEC3D v, axis;
+  MAT3D rot, m;
+  double angle;
+
+  printf("---- TEST 3D MATH_LIB ----\n");
+
+  v = set_vec3d(1.0, 1.0, 0.0);
+
+  m = set_mat3d(1.0, 0.0, 0.0, 
+                0.0, 2.0, 0.0, 
+                0.0, 0.0, 3.0);
+
+  angle = M_PI / 2;
+  axis = set_vec3d(1.0, 0.0, 0.0);
+  rot = make_rotation(axis, angle);
+
+  printf("angle = %5.2f\n", angle*180.0 / M_PI);
+  print_vec3d(axis, "n");
+  print_mat3d(rot, "rot");
+  print_mat3d(transpose_mat3d(rot), "rot (test transpose)");
+  print_vec3d(v, "\nv");
+  print_vec3d(rotate_vec3d(v, rot), "v_rotated");
+  print_mat3d(m, "\nm");
+  print_mat3d(rotate_mat3d(m, rot), "m_rotated");
+  print_mat3d(product_mat3d(rot, product_mat3d(m, transpose_mat3d(rot))), "m_rotated (test product)");
+
+
+  return 0;
+}
